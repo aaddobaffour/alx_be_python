3b31e2f82bed6124d406ba45d3ba0813f450f113
aaddobaffour <aaddo_baffour@st.ug.edu.gh>
2025-09-19 08:13:57 +0000

    Add files via upload

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,24 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+operator = input("Choose the operation (+, -, *, /):")
+match operator:
+    case "+":
+        result = num1 + num2
+        print(f'The result is {result}')
+    case "-":
+        result = num1 - num2
+        print(f'The result = {result}')
+    case "*":
+        result = num1 * num2
+        print(f'The result is {result}')
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+            print(f'The result is {result}')
+        else:
+            print("Error: Can't divede by zero.")
+    case _:
+        print("Invalid operator. Please choose from +, -, *, /.")
+              
+
+
